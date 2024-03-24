bb756c4424ea65f5a7e4edbd375e08b279f12951
Adisak Jantra Sopark <ajsopark@gmail.com>
2024-03-24 14:50:28 +0700

    add3

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 # Core Pkgs
 import streamlit as st
 import numpy as np
+import os
+import time
+timestr = time.strftime("%Y%M%D - %H%M%S")
+
+
+# For QR Code
+import qrcode
+
+qr = qrcode.QRCode(version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=14,
+    )
+
+from PIL import Image
+# Function to Load Image
+def load_image(img):
+    im = Image.open(img)
+    return im
 
 
 # Application
@@ -10,8 +29,38 @@ def main():
     choice = st.sidebar.selectbox("Menu",menu)
 
     if choice == "Home":
-        
+        st.subheader("Home")
+        # Text Input
 
+    elif choice == "DecodeQR":
+        st.subheader("Decode QR")
+        with st.form(key='myqr_form'):
+            raw_text = st.text_area("กรอกข้อมูล")
+            submit_button = st.form_submit_button("สร้าง QR Code")
+
+        # Layout
+        if submit_button:
+            col1, col2 = st.columns(2)
+
+            with col1:
+                # Add Date
+                qr.add_data(raw_text)
+                # Generate
+                qr.make(fit=True)
+                img = qr.make_image(fill_color='black',back_color='white')
+
+                #fillname
+                img_filename = 'generate_image_{}.png'.format(timestr)
+                path_for_images = os.path.join('qr_finish', img_filename)
+                img.save(path_for_images)
+
+            with col2:
+                st.info("ข้อมูลต้นฉบับ")
+                st.write(raw_text)
+
+
+    else:
+        st.subheader("About")
 
 if __name__ == '__main__':
     main()
